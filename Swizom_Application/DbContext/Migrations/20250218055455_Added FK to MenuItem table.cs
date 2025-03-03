28b5e01d488ab9e9c1ae0e49c4173b7bc88bb776
TejaswiKuppili <78371931+TejaswiKuppili@users.noreply.github.com>
2025-03-03 19:36:01 +0530

    DbContext

---
+++ Swizom_Application/DbContext/Migrations/20250218055455_Added FK to MenuItem table.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SwizomDbContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFKtoMenuItemtable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RestaurantID",
+                table: "MenuItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItems_RestaurantID",
+                table: "MenuItems",
+                column: "RestaurantID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuItems_Restaurants_RestaurantID",
+                table: "MenuItems",
+                column: "RestaurantID",
+                principalTable: "Restaurants",
+                principalColumn: "RestaurantID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuItems_Restaurants_RestaurantID",
+                table: "MenuItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MenuItems_RestaurantID",
+                table: "MenuItems");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantID",
+                table: "MenuItems");
+        }
+    }
+}
---
+++ Swizom_Application/DbContext/Models/MenuCategory.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SwizomDbContext.Models
+{
+    public class MenuCategory
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        [ForeignKey("Restaurant")]
+        public int RestaurantID { get; set; }
+        public Restaurant Restaurant { get; set; }
+
+        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+    }
+}
